0bce19e9aff225eed1d95e0ed117b80ab230fb62
pwmurawski <pwmurawski123@gmail.com>
2022-12-18 03:58:13 +0100

    add

--- pages/foodProducts/index.tsx
+++ pages/foodProducts/index.tsx
@@ -1,36 +1,36 @@
-import { GetStaticProps } from "next";
-import AddNewFoodProduct from "../../components/AddNewFoodProduct/AddNewFoodProduct";
-import FoodProductsTable from "../../components/FoodProductsTable/FoodProductsTable";
-import getFoodProducts from "../../api/getFoodProducts";
-import { IFoodProductData } from "../../types/IFoodProductDataTypes";
-import useAuth from "../../hooks/useAuth";
+// import { GetStaticProps } from "next";
+// import AddNewFoodProduct from "../../components/AddNewFoodProduct/AddNewFoodProduct";
+// import FoodProductsTable from "../../components/FoodProductsTable/FoodProductsTable";
+// import getFoodProducts from "../../api/getFoodProducts";
+// import { IFoodProductData } from "../../types/IFoodProductDataTypes";
+// import useAuth from "../../hooks/useAuth";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const response = await getFoodProducts();
-  if (!response?.data)
-    throw new Error(
-      `Failed to fetch data, received status ${response?.status}`
-    );
+// export const getStaticProps: GetStaticProps = async () => {
+//   const response = await getFoodProducts();
+//   if (!response?.data)
+//     throw new Error(
+//       `Failed to fetch data, received status ${response?.status}`
+//     );
 
-  return {
-    props: {
-      foodProducts: response.data,
-    },
-  };
-};
+//   return {
+//     props: {
+//       foodProducts: response.data,
+//     },
+//   };
+// };
 
-export default function FoodProducts({
-  foodProducts,
-}: {
-  foodProducts: IFoodProductData[];
-}) {
-  const { isUser, logoutHandler } = useAuth();
-  if (!isUser) logoutHandler();
+// export default function FoodProducts({
+//   foodProducts,
+// }: {
+//   foodProducts: IFoodProductData[];
+// }) {
+//   const { isUser, logoutHandler } = useAuth();
+//   if (!isUser) logoutHandler();
 
-  return (
-    <>
-      <FoodProductsTable foodProductsData={foodProducts} />
-      <AddNewFoodProduct />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <FoodProductsTable foodProductsData={foodProducts} />
+//       <AddNewFoodProduct />
+//     </>
+//   );
+// }

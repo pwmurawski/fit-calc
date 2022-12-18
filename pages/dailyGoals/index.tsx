585044e2648d2d101f6ac9d8676acb02fb82aed9
pwmurawski <pwmurawski123@gmail.com>
2022-12-18 14:51:47 +0100

    add

--- pages/dailyGoals/index.tsx
+++ pages/dailyGoals/index.tsx
@@ -1,7 +1,21 @@
 import { useState } from "react";
 import DailyGoalsForm from "../../components/Forms/DailyGoalsForm/DailyGoalsForm";
 import SummaryDailyGoals from "../../components/SummaryDailyGoals/SummaryDailyGoals";
+import userAuth from "../../helpers/userAuth";
 import useEditDailyGoals from "../../hooks/useEditDailyGoals";
+import { IGetServerProps } from "../../types/GetServerPropsTypes";
+
+export const getServerSideProps = async ({ req, res }: IGetServerProps) => {
+  const { isUser } = userAuth(req, res);
+  if (!isUser)
+    return {
+      redirect: {
+        destination: "/login",
+      },
+    };
+
+  return { props: {} };
+};
 
 export default function DailyGoals() {
   const [totalPercent, setTotalPercent] = useState(0);
